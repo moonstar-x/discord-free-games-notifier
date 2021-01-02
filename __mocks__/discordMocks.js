4480f1ac1358b9cdf09f4e43b3b2f11081fbdc03
moonstar-x <christian.lopez99@outlook.com>
2021-01-02 01:02:59 -0500

    Created ExtendedCommand

--- __mocks__/discordMocks.js
+++ __mocks__/discordMocks.js
@@ -2,6 +2,29 @@ const guildMock = {
   name: 'guild'
 };
 
+const clientMock = {
+  handleCommandError: jest.fn()
+};
+
+const userMock = {
+  username: 'user'
+};
+
+const memberMock = {
+  displayName: 'nickname'
+};
+
+const messageMock = {
+  reply: jest.fn(),
+  guild: guildMock,
+  author: userMock,
+  member: memberMock
+};
+
 module.exports = {
-  guildMock
+  guildMock,
+  clientMock,
+  messageMock,
+  userMock,
+  memberMock
 };
