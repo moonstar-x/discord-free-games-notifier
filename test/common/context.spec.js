fc077c00f1abccc4bfb24672705717b7f714f128
moonstar-x <christian.lopez99@outlook.com>
2021-01-03 23:00:56 -0500

    Added devMode to context.

--- test/common/context.spec.js
+++ test/common/context.spec.js
@@ -24,4 +24,22 @@ describe('Common - Context', () => {
       process.argv = oldArgs;
     });
   });
+
+  describe('devMode', () => {
+    it('should return true if NODE_ENV is set to development.', () => {
+      process.env.NODE_ENV = 'development';
+      jest.resetModules();
+      context = require('../../src/common/context');
+
+      expect(context.devMode).toBe(true);
+    });
+
+    it('should return false if NODE_ENV is not set to development.', () => {
+      process.env.NODE_ENV = 'production';
+      jest.resetModules();
+      context = require('../../src/common/context');
+
+      expect(context.devMode).toBe(false);
+    });
+  });
 });
