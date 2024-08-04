440d4c85db3e59f846f863def91477e699670900
moonstar-x <christian.lopez99@outlook.com>
2024-08-04 09:57:50 -0500

    Implemented getStorefronts.

---
+++ src/features/gameOffers/functions/getStorefronts.spec.ts
@@ -0,0 +1,25 @@
+import { getStorefronts } from './getStorefronts';
+import { db } from '../../../services/database/client';
+
+jest.mock('../../../services/database/client');
+
+describe('Features > GameOffers > Functions > GetStorefronts', () => {
+  const storefronts = ['EpicGames', 'Steam'];
+
+  beforeAll(() => {
+    (db.one as jest.Mock).mockResolvedValue({ result: storefronts });
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it('should be defined.', () => {
+    expect(getStorefronts).toBeDefined();
+  });
+
+  it('should return the result.', async () => {
+    const result = await getStorefronts();
+    expect(result).toBe(storefronts);
+  });
+});
