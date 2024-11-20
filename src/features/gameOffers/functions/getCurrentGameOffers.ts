e1d930f6e5f88b0c5bdcb555bd1a773ff785191b
moonstar-x <christian.lopez99@outlook.com>
2024-11-20 15:54:20 -0500

    Added keys check before calling redis client.mget.

--- src/features/gameOffers/functions/getCurrentGameOffers.ts
+++ src/features/gameOffers/functions/getCurrentGameOffers.ts
@@ -4,8 +4,11 @@ import { GameOffer } from '../../../models/gameOffer';
 export const getCurrentGameOffers = (): Promise<GameOffer[]> => {
   return withRedis(async (client) => {
     const keys = await client.keys('offer:*');
-    const items = await client.mGet(keys);
+    if (!keys.length) {
+      return [];
+    }
 
+    const items = await client.mGet(keys);
     return items
       .filter((item) => item !== null)
       .map((item: string) => JSON.parse(item));
